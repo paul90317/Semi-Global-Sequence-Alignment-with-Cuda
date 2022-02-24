1e135574edc8d903c8a8a61e7f10cc293c87e24c
paul <paul2309825@gmail.com>
2022-02-24 19:59:21 +0800

    trace unit copy one by one.

--- src/headers/utils.h
+++ src/headers/utils.h
@@ -5,6 +5,7 @@
 #define max2(a,b) (((a)>(b))?(a):(b))
 #define max3(a,b,c) max2((a),(max2((b),(c))))
 typedef unsigned char byte;
+typedef long long unsigned size_2D;
 template<typename T>
 T* oalloc(int count){
     return (T*)malloc(count*sizeof(T));
